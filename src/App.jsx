804661c8bf7e7bf9392a75706c7456bac950fa08
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-04-08 11:49:10 +1000

    added gradient background to loading page

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,17 @@ function App() {
   
   if (isChecking) return <div className="min-h-screen bg-[#020617] text-[#F5EDE6] flex flex-col items-center justify-center font-semibold space-y-4 relative overflow-hidden"
   >
+
+    <div
+    className="
+      absolute 
+      inset-0 z-0 
+      pointer-events-none"
+      style={{
+        backgroundImage: `radial-gradient(circle 500px at 50% 100px, rgba(239,68,68,0.4), transparent)`,
+      }}
+  />
+  
     <p>Please wait while the server loads.</p>
     <p>
       <a
